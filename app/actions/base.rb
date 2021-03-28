3a1d1d5162e9dbc27d94a9095a82b8b65fdc000c
Daniel Portales <dportalesr@gmail.com>
2021-03-27 19:19:07 -0600

    Disable exception handling for non-production envs

--- app/actions/base.rb
+++ app/actions/base.rb
@@ -3,6 +3,12 @@
 require "hanami/controller"
 require "hanami/validations"
 
+Hanami::Controller.configure do
+  handle_exceptions ENV["RACK_ENV"] =~ /production/
+  default_request_format :json
+  default_response_format :json
+end
+
 module App
   module Actions
     class Base
