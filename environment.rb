cc331e64d73a26b1091efc94d7f925ebde9adc01
Daniel Portales <dportalesr@gmail.com>
2021-03-28 08:57:38 -0600

    Use dummy storage (memory) for Rack attack

--- environment.rb
+++ environment.rb
@@ -14,4 +14,6 @@ Raddocs.configure do |c|
   c.docs_dir = Pathname.new File.join(APP_ROOT, "docs")
 end
 
+# TODO: Implement *real* storage
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 Rack::Attack.throttle("requests by ip", limit: 5, period: 2) { |req| req.ip }
