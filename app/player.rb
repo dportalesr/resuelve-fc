6c6342ab68c652b78e3134d9635fffcf0fb1310b
Daniel Portales <dportalesr@gmail.com>
2021-03-27 19:06:43 -0600

    Refactor: separate model validations from payload schema validations

--- app/player.rb
+++ app/player.rb
@@ -5,19 +5,52 @@ require_relative "team"
 
 module App
   class Player < Base
+    class Payload
+      include Hanami::Validations
+
+      validations do
+        required(:nombre).filled(:str?)
+        required(:nivel).filled(:str?, included_in?: App::Team::LEVELS)
+        required(:goles).filled(:int?)
+        required(:sueldo).filled(:int?)
+        required(:bono).filled(:int?)
+        required(:equipo).filled(:str?)
+
+        optional(:sueldo_completo)
+      end
+    end
+
     include Hanami::Validations
 
-    attr_accessor :nombre, :nivel, :goles, :sueldo, :bono, :sueldo_completo, :equipo
+    attr_accessor :name, :level, :score, :base_salary, :bonus, :team
 
     validations do
-      required(:nombre).filled(:str?)
-      required(:nivel).filled(:str?, included_in?: App::Team::LEVELS)
-      required(:goles).filled(:int?)
-      required(:sueldo).filled(:int?)
-      required(:bono).filled(:int?)
-      required(:equipo).filled(:str?)
+      required(:name).filled(:str?)
+      required(:level).filled(:str?, included_in?: App::Team::LEVELS)
+      required(:score).filled(:int?)
+      required(:base_salary).filled(:decimal?)
+      required(:bonus).filled(:decimal?)
+      required(:team).filled
+    end
 
-      optional(:sueldo_completo).filled(:int?)
+    def initialize(atts = {})
+      @name = atts[:nombre]
+      @level = atts[:nivel]
+      @score = atts[:goles].to_i
+      @base_salary = atts[:sueldo].to_f
+      @bonus = atts[:bono].to_f
+      @team = atts[:team]
+    end
+
+    def to_payload
+      {
+        nombre: name,
+        nivel: level,
+        goles: score,
+        sueldo: base_salary,
+        bono: bonus,
+        equipo: team.name
+      }
     end
   end
 end
--- app/team.rb
+++ app/team.rb
@@ -4,18 +4,40 @@ require_relative "base"
 
 module App
   class Team < Base
-    include Hanami::Validations
-
     LIST = %w[rojo azul negro amarillo].freeze
     LEVELS = %w[A B C Cuauh].freeze
+    DEFAULT_TABULATOR = {
+      "A" => 5,
+      "B" => 10,
+      "C" => 15,
+      "Cuauh" => 20
+    }.freeze
 
-    validations do
-      required(:equipo).filled(:str?, included_in?: LIST)
-      required(:niveles).schema do
-        LEVELS.each do |level|
-          required(level.to_sym).filled(:int?, gt?: 0)
+    class Tabulator
+      include Hanami::Validations
+
+      validations do
+        required(:equipo).filled(:str?, included_in?: App::Team::LIST)
+        required(:niveles).schema do
+          LEVELS.each do |level|
+            required(level.to_sym).filled(:int?, gt?: 0)
+          end
         end
       end
+    end
+
+    include Hanami::Validations
+
+    attr_accessor :name, :tabulator, :players
+
+    validations do
+      required(:name).filled(:str?, included_in?: LIST)
+    end
+
+    def initialize(atts = {})
+      @name = atts[:name]
+      @tabulator = atts[:tabulator] || DEFAULT_TABULATOR
+      @players = []
     end
   end
 end
